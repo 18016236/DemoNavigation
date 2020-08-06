a865492f174f0817233633d5f91ae4a5657b407b
18016236 <18016236@myrp.edu.sg>
2020-08-06 10:14:59 +0800

    commit

--- app/src/main/java/com/example/demonavigation/MainActivity.java
+++ app/src/main/java/com/example/demonavigation/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
     Button btnNewActivity;
     @Override
@@ -27,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
                                                   });
 
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
